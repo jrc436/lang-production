7bbcd5a084867f9245ede0d837478389f9763342
Jeremy Cole <jrc436@psu.edu>
2015-01-29 16:06:07 -0500

    white eclipse project added

---
+++ candc/src/lib/version.cc
@@ -0,0 +1,5 @@
+namespace NLP {
+  const char *VERSION = "v2545";
+
+  const char *BUILD = "(unix build on 5 December 2014, 20:59:23)";
+}
---
+++ candc/src/prolog/boxer/version.pl
@@ -0,0 +1,2 @@
+:- module(version,[version/1]).
+version('boxer v2545 (unix build on 5 December 2014, 20:55:54)').

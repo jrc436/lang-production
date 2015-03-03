2eca0a1ed8f5ef709cc6883633ebff91f9f0d98c
Jeremy Cole <jrc@dreitter-imac.ist.psu.edu>
2015-03-02 23:54:48 -0500

    basic changing that shouldn't interfere with local build

--- openccg/actup-realizer/src/opennlp/ccg/ClientTerminal.java
+++ openccg/actup-realizer/src/opennlp/ccg/ClientTerminal.java
@@ -1,13 +1,18 @@
 package opennlp.ccg;
 
-
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 //forgot to commit a simple file and don't want to cause git collisions
 
 public class ClientTerminal {
-	private static final String[] filePaths = new String[] {
-		"../../../../ap-largefiles/data/swbd/swbd-rawtext-full.txt",
-	};
+	private static final String pathToLoadAll = "../../../../ap-largefiles/data/run-list";
+	private static final String prefix = "../../../../ap-largefiles/data/swbd/";
+	//private static final String[] filePaths = new String[] {
+	//	"../../../../ap-largefiles/data/swbd/swbd-rawtext-full.txt",
+	//};
 	public static final String trainingPath = "../../../../ap-largefiles/data/wsj/wsj-lm";
 	
 	public static final String defaultScorer = "opennlp.ccg.ngrams.NgramScorer";
@@ -17,11 +22,11 @@ public class ClientTerminal {
 	public static final int nBestListSize = 1;
 	
 	private static final String grammar = "../../config/grammar/grammar.xml";
-	public static final boolean USEACTR = true;
+	public static final boolean USEACTR = false;
 	
 	public static void main(String[] args) throws Exception {
 		Realize r = new Realize();
-		//Parse p = new Parse();
+		Parse p = new Parse();
 		
 		boolean useACTR = USEACTR;
 		if (args.length > 0) {
@@ -34,10 +39,13 @@ public class ClientTerminal {
 		}	
 		
 		String addendum = useACTR ? "-actr" : "-std";
-		for (String in : filePaths) {
-			String out1 = in.split(".txt")[0] + "-parsed.txt";
+		
+		List<String> filePaths = Files.readAllLines(Paths.get(pathToLoadAll), Charset.defaultCharset());
+		for (String fp : filePaths) {
+			String in = prefix + fp;
+			String out1 = in.split(".txt")[0] + "-parsed.xml";
 			String out2 = in.split(".txt")[0] + "-out"+addendum+".txt";
-			//p.parseMain(grammar, in, out1, defaultScorer, defaultConfig, nBestListSize);
+			p.parseMain(grammar, in, out1, defaultScorer, defaultConfig, nBestListSize);
 			r.realizeMain(useACTR, trainingPath, grammar, out1, out2);
 		}
 	}
